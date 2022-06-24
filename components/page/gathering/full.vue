543ba79582fded2a2f27c9252429152ec86d4568
TheotimeColin <colin.theotime@gmail.com>
2022-06-24 09:56:49 +0200

    tweak(EventId): improve layout

--- components/page/gathering/full.vue
+++ components/page/gathering/full.vue
@@ -1,42 +1,51 @@
 <template>
     <div>
-        <popin :is-active="isActive" :modifiers="['panel']" :title="'Détails de : ' + gathering.title" query="full" @close="$emit('close')" @open="$emit('open')">
+        <popin :is-active="isActive" :modifiers="['panel']" :title="'Détails de : ' + gathering.title" query="full"
+            @close="$emit('close')" @open="$emit('open')">
             <div class="p-30 p-20@s" slot="content">
                 <transition name="fade">
                     <div v-show="!isManage">
-                        <div class="+mt-30 block-r">
-                            <div class="d-flex">
-                                <fa icon="far fa-calendar" class="mt-5 mr-5 fx-no-shrink" fixed-width /> 
-                                
-                                <p>{{ $moment(gathering.date).format('ddd D MMMM YYYY à HH:mm') }}</p>
-                            </div>
-                            <div class="mt-5 d-flex">
-                                <fa icon="far fa-map-marker-alt" class="mt-5 mr-5 fx-no-shrink" fixed-width /> 
+                        <div class="+mt-30">
+                            <div class="d-flex fxa-center">
+                                <fa icon="fal fa-calendar-lines" size="xl" class="mt-5 mr-10 fx-no-shrink" fixed-width />
 
                                 <div>
-                                    <p class="ft-bold">{{ gathering.location }}</p>
-                                    <p class="color-ft-weak">{{ gathering.address }}</p>
+                                    <p class="ft-l-bold">{{ $moment(gathering.date).format('dddd D MMMM YYYY') }}</p>
+                                    <p>à partir de {{ $moment(gathering.date).format('HH:mm') }}</p>
+                                </div>
+                            </div>
+
+                            <div class="d-flex fxa-center mt-15">
+                                <fa icon="fal fa-map-marker-alt" size="xl" class="mt-5 mr-10 fx-no-shrink" fixed-width />
+
+                                <div>
+                                    <p class="ft-l-bold">{{ gathering.location }}</p>
+                                    <p>{{ gathering.address }}</p>
                                 </div>
                             </div>
                         </div>
+
+                        <hr class="Separator mv-30">
 
                         <template v-if="user">
                             <div class="+mt-30" v-if="gathering.description && gathering.description != '<p></p>'">
                                 <h2 class="ft-title-xs mb-5">Détails</h2>
                                 <text-body :modifiers="['gathering']" :value="gathering.description" />
                             </div>
-                            
-                            <div class="+mt-30 p-20 bg-bg-weak br-s" v-if="gathering.important && gathering.important != '<p></p>'">
+
+                            <div class="+mt-30 p-20 bg-bg-weak br-s"
+                                v-if="gathering.important && gathering.important != '<p></p>'">
                                 <h2 class="ft-title-2xs mb-15 tape">Important</h2>
                                 <text-body :modifiers="['gathering']" :value="gathering.important" />
                             </div>
 
-                            <div class="+mt-30 block" v-if="organizers && organizers.length > 0">
+                            <div class="+mt-30 block-r" v-if="organizers && organizers.length > 0">
                                 <h2 class="ft-title-xs mb-10">Équipe d'organisation</h2>
 
                                 <div class="row-xs">
                                     <div class="col-6 col-12@xs mt-10" v-for="user in organizers" :key="user._id">
-                                        <user-icon v-bind="user" :display-name="true" class="mr-10 fx-no-shrink" :modifiers="['m']" />
+                                        <user-icon v-bind="user" :display-name="true" class="mr-10 fx-no-shrink"
+                                            :modifiers="['m']" />
                                     </div>
                                 </div>
                             </div>
@@ -47,13 +56,11 @@
                             <insert-register />
                         </template>
 
-                        <div class="+mt-30 pv-20 fx-center bg-bg b-top" style="position: sticky; bottom: 0;" v-if="user && !hasWaitingList && !gathering.isPast">
+                        <div class="+mt-30 pv-20 fx-center bg-bg b-top" style="position: sticky; bottom: 0;"
+                            v-if="user && !hasWaitingList && !gathering.isPast">
                             <div></div>
 
-                            <page-gathering-action-button
-                                :gathering="gathering"
-                                @manage="isManage = true"
-                            />
+                            <page-gathering-action-button :gathering="gathering" @manage="isManage = true" />
                         </div>
                     </div>
                 </transition>
@@ -61,11 +68,13 @@
                     <div v-show="isManage">
                         <template v-if="hasBooked">
                             <p>Ton inscription est confirmée. Rendez-vous {{ $moment(gathering.date).format('dddd DD MMMM YYYY à HH:mm') }} !</p>
-                            
-                            <div class="block mt-20">
-                                <p>Tu as dû recevoir un email de confirmation avec un récapitulatif des informations. Vérifie tes spam !</p>
 
-                                <link-base class="mt-15" :href="googleCal" icon-before="calendar" target="_blank">Ajouter à Google Calendar</link-base>
+                            <div class="block mt-20">
+                                <p>Tu as dû recevoir un email de confirmation avec un récapitulatif des informations.
+                                    Vérifie tes spam !</p>
+
+                                <link-base class="mt-15" :href="googleCal" icon-before="calendar" target="_blank">
+                                    Ajouter à Google Calendar</link-base>
                             </div>
 
                             <div class="block-r bg-bg-strong mt-20">
@@ -74,14 +83,16 @@
                                 </h2>
 
                                 <div>
-                                    <b>L'équipe de {{ constellation.name }}</b> se donne beaucoup de mal pour organiser ces sorties gratuites !
+                                    <b>L'équipe de {{ constellation.name }}</b> se donne beaucoup de mal pour organiser
+                                    ces sorties gratuites !
 
                                     <p class="mt-15">
                                         Donc si tu ne peux plus venir, <u>pense bien à te désinscrire à l'avance.</u>
                                     </p>
                                 </div>
-                                
-                                <button-base class="mt-15" icon-before="times" :loading="isLoading" @click="onBookUpdate('cancelled')">
+
+                                <button-base class="mt-15" icon-before="times" :loading="isLoading"
+                                    @click="onBookUpdate('cancelled')">
                                     Me désinscrire
                                 </button-base>
                             </div>
@@ -90,18 +101,22 @@
                             <div class="block">
                                 <p class="ft-title-xs mb-10">Avant de confirmer...</p>
 
-                                <b>L'équipe de {{ constellation.name }}</b> comptent sur ta venue le jour J ! Les membres qui ne viennent pas, sans prévenir ou se désinscrire, peuvent être exclus du groupe ou même de la plateforme.
+                                <b>L'équipe de {{ constellation.name }}</b> comptent sur ta venue le jour J ! Les
+                                membres qui ne viennent pas, sans prévenir ou se désinscrire, peuvent être exclus du
+                                groupe ou même de la plateforme.
                             </div>
                         </template>
-                    
-                        <div class="pv-20 fx-center bg-bg b-top mt-20 fx-reverse@xs" style="position: sticky; bottom: 0;">
+
+                        <div class="pv-20 fx-center bg-bg b-top mt-20 fx-reverse@xs"
+                            style="position: sticky; bottom: 0;">
                             <div class="mt-5@xs">
                                 <link-base icon-before="arrow-left" @click="isManage = false">
                                     Retour à la description
                                 </link-base>
                             </div>
 
-                            <button-base :modifiers="['light']" :loading="isLoading" @click="onBookUpdate('attending')" icon-before="check" v-if="!hasBooked">
+                            <button-base :modifiers="['light']" :loading="isLoading" @click="onBookUpdate('attending')"
+                                icon-before="check" v-if="!hasBooked">
                                 Confirmer mon inscription
                             </button-base>
                             <button-base :modifiers="['light']" @click="$emit('close')" v-else>
--- components/page/gathering/manage.vue
+++ components/page/gathering/manage.vue
@@ -13,9 +13,7 @@
                     </div>
                 </div>
 
-                <hr class="Separator mv-15">
-
-                <div class="d-flex fxa-center">
+                <div class="d-flex fxa-center mt-15">
                     <fa icon="fal fa-map-marker-alt" size="xl" class="mt-5 mr-10 fx-no-shrink" fixed-width />
 
                     <div>
@@ -34,15 +32,6 @@
                     <link-base class="mt-5" @click="isFull = true">Voir plus</link-base>
                 </div>
 
-                <hr class="Separator mv-15">
-
-                <div class="d-flex fxa-center" v-if="organizers && organizers.length > 0">
-                    <user-icon v-bind="organizers[0]" class="mr-10 fx-no-shrink" :modifiers="['s']" />
-
-                    <p class="ft-m-medium">Organisé par {{ $pluralize(organizers.map(o => o.name)) }}</p>
-                </div>
-
-
                 <hr class="Separator mt-15">
             </div>
             <div class="fx-center ph-20 pb-20 d-block@xs"
@@ -56,7 +45,9 @@
                         <button-base :modifiers="['light']" disabled>Événement terminé</button-base>
                     </template>
                     <template v-else>
-                        <link-base class="mr-5" :to="{ name: 'c-slug-manage-events-id', params: { slug: constellation.slug, id: gathering._id } }" v-if="$isConsteOrga">Modifier</link-base>
+                        <link-base class="mr-5"
+                            :to="{ name: 'c-slug-manage-events-id', params: { slug: constellation.slug, id: gathering._id } }"
+                            v-if="$isConsteOrga">Modifier</link-base>
                         <link-base class="mr-5" @click="isFull = true">Détails</link-base>
 
                         <page-gathering-action-button :gathering="gathering" @manage="isFull = true" />
@@ -70,7 +61,8 @@
 
                 <slider-block
                     :slots="usersByStatus(['confirmed', 'attending']).filter(u => u._id != user._id).map(u => u._id)"
-                    :ratio="150" item-class="width-2xss" :margin="8" :offset="$smallerThan('xs') ? 15 : 20" :offset-v="20">
+                    :ratio="150" item-class="width-2xss" :margin="8" :offset="$smallerThan('xs') ? 15 : 20"
+                    :offset-v="20">
                     <div v-for="user in usersByStatus(['confirmed', 'attending']).filter(u => u._id != user._id)"
                         :slot="user._id" :key="user._id">
                         <user-profile v-bind="user" :no-link="true" :gathering="gathering._id"
@@ -88,7 +80,18 @@
         <popin :is-active="isList" :modifiers="['s']" @close="isList = false">
             <template slot="content">
                 <div class="p-30">
-                    <p class="ft-title-s mb-15">Participations</p>
+                    <div class="+mt-30" v-if="organizers && organizers.length > 0">
+                        <h2 class="ft-title-xs mb-10">Équipe d'organisation</h2>
+
+                        <div class="row-xs">
+                            <div class="col-6 col-12@xs mt-10" v-for="user in organizers" :key="user._id">
+                                <user-icon v-bind="user" :display-name="true" class="mr-10 fx-no-shrink"
+                                    :modifiers="['m']" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <p class="+mt-30 ft-title-xs mb-5">Participations</p>
 
                     <div class="row-xs">
                         <div class="col-6 pt-10 col-12@xs"
