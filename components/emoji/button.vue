d3c827b1372c1b9f007dc8ed12100e16f9887785
TheotimeColin <colin.theotime@gmail.com>
2022-06-30 10:11:32 +0200

    fix(Emoji): fix emoji selector

--- components/emoji/button.vue
+++ components/emoji/button.vue
@@ -9,7 +9,7 @@
             @mouseleave="isActive = null"
         />
 
-        <div class="EmojiButton_popin" :class="{ 'is-active': isActive, 'is-disabled': isActive === false }" @mouseenter="isActive = true" @mouseleave="isActive = null" >
+        <div class="EmojiButton_popin" :class="{ 'is-active': isActive, 'is-disabled': isActive === false }" @mouseenter="isActive = true" @mouseleave="isActive = null" v-if="$biggerThan('s')">
             <div class="EmojiButton_popinContainer bg-bg-strong br-s shadow">
                 <reaction-emoji-selector :is-active="isActive" @input="(v) => $emit('input', v)" />
             </div>
