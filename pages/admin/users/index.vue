92809c2fa89b5acfa1c8369d2224ee176bf942b7
TheotimeColin <colin.theotime@gmail.com>
2022-07-02 01:13:02 +0200

    feat(Admin): add user info

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -7,7 +7,7 @@
                 </div>
                 
                 <div class="ft-m p-15 mv-10 bg-bg-weak br-s" v-for="user in users" :key="user._id">
-                    <div class="d-flex fxa-center">
+                    <div class="d-flex fxa-center +mt-10">
                         <user-icon class="fx-no-shrink mr-10" v-bind="user" />
 
                         <div class="fx-grow">
@@ -24,14 +24,31 @@
                             </div>
                         </div>
                         <div class="fx-no-shrink ml-20">
-                            <span class="mr-10">
-                                {{ user.ref ? user.ref : 'Unknown' }}
-                            </span>
-                            <span class="mr-10" v-if="user.referral">
-                                {{ user.referral }}
-                            </span>
                             <span>{{ user.id }}</span>
                             <span class="color-ft-weak">{{ $moment(user.createdAt).format('DD MMM hh:mm') }}</span>
+                        </div>
+                    </div>
+                    <div class="p-10 bg-bg-strong +mt-10" v-if="user.friends.length > 0">
+                        Amis
+                        <div class="row-xs">
+                            <div class="col-6 p-5" v-for="friend in user.friends" :key="friend._id">
+                                <user-icon :display-name="true" v-bind="$getUser(friend)" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-10 bg-bg-strong +mt-10" v-if="user.affinities.length > 0">
+                        Demandes envoyées
+                        <div class="row-xs">
+                            <div class="col-6 p-5" v-for="affinity in user.affinities" :key="affinity._id">
+                                <user-icon :display-name="true" v-bind="$getUser(affinity)" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-10 bg-bg-strong +mt-10" v-if="$store.getters['mention/find']({ owner: user._id }).length > 0">
+                        Mentions envoyées
+
+                        <div class="mt-5" v-for="mention in $store.getters['mention/find']({ owner: user._id })" :key="mention._id">
+                            <span class="tape tape-s ft-xs-bold">{{ mention.type }}</span> {{ $getUser(mention.target).name }} @{{ $getUser(mention.target).id }}
                         </div>
                     </div>
                 </div>
@@ -49,7 +66,8 @@ export default {
     name: 'NewsletterPage',
     layout: 'admin',
     async fetch () {
-        let response = await this.$store.dispatch('user/fetch')
+        await this.$store.dispatch('user/fetch')
+        await this.$store.dispatch('mention/fetch')
     },
     data: () => ({
         data: []
@@ -60,7 +78,12 @@ export default {
                 sort: { createdAt: 'asc' },
                 name: '$notNull'
             })
-        }
-    }
+        },
+        mentions () {
+            return this.$store.getters['mention/find']({
+                sort: { createdAt: 'asc' }
+            })
+        },
+    },
 }
 </script>
