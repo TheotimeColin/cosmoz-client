23b817dc91800548e13e8878a1b0a1ff5e20c25d
TheotimeColin <colin.theotime@gmail.com>
2022-07-07 15:15:23 +0200

    fix(Reactions)

--- components/content/reaction-popin.vue
+++ components/content/reaction-popin.vue
@@ -2,19 +2,19 @@
     <popin :is-active="isActive" :modifiers="['s']" query="reactions" @close="$emit('close')">
         <template slot="content">
             <div class="p-20">
-                <p class="ft-title-xs mb-20"><span class="mr-5 round-s bg-bg-strong" v-if="reactions">{{ reactions.reduce((t, c) => t += c[1].length, 0) }}</span> Réactions</p>
+                <p class="ft-title-xs mb-20"><span class="mr-5 round-s bg-bg-strong" v-if="reactions">{{ reactions.reduce((t, c) => t += c[1].items.length, 0) }}</span> Réactions</p>
 
                 <div v-if="reactions">
                     <div class="+mt-30" v-for="reaction in reactions" :key="reaction[0]"> 
                         <div class="fx-center mb-10">
                             <div class="ft-xl-medium">{{ reaction[0] }}</div>
-                            <div class="round-xs bg-bg-strong ml-3">{{ reaction[1].length }}</div>
+                            <div class="round-xs bg-bg-strong ml-3">{{ reaction[1].items.length }}</div>
 
                             <hr class="Separator ml-10">
                         </div>
 
 
-                        <div class="+mt-5" v-for="r in reaction[1]" :key="r.owner">
+                        <div class="+mt-5" v-for="r in reaction[1].items" :key="r.owner">
                             <user-icon v-bind="getOwner(r.owner)" :modifiers="['s']" :display-name="true" />
                         </div>
                     </div>
